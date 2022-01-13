b6dd7ef4fc48fc50fb7963efef10d26ed43c4487
cindy_chao <cindy_chao@mail.chungyo.net>
2022-01-13 16:05:36 +0800

    add: Highchart.vue

---
+++ src/components/HighChart.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <highcharts :options="chartOptions" :callback="myCallback"></highcharts>
+  </div>
+</template>
+<script>
+const chart = {
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Global",
+  },
+  yAxis: {
+    title: {
+      text: "Number",
+    },
+  },
+  legend: {
+    layout: "vertical",
+    align: "right",
+    verticalAlign: "middle",
+  },
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false,
+      },
+      //   pointStart: 2010,
+    },
+  },
+  series: [
+    {
+      name: "NewConfirmed",
+      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+    },
+    {
+      name: "NewDeaths",
+      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+    },
+    {
+      name: "NewRecovered",
+      data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+    },
+    {
+      name: "TotalConfirmed",
+      data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+    },
+    {
+      name: "TotalDeaths",
+      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+    },
+    {
+      name: "TotalRecovered",
+      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+    },
+  ],
+  responsive: {
+    rules: [
+      {
+        condition: {
+          maxWidth: 500,
+        },
+        chartOptions: {
+          legend: {
+            layout: "horizontal",
+            align: "center",
+            verticalAlign: "bottom",
+          },
+        },
+      },
+    ],
+  },
+};
+export default {
+  props: {
+    data: {
+      type: Object,
+      require: true,
+    },
+  },
+  name: "HelloWorld",
+  data() {
+    return {
+      chartOptions: chart,
+    };
+  },
+  mounted() {},
+  methods: {
+    myCallback() {
+      console.log(this.data);
+      console.log("this is callback function");
+    },
+  },
+};
+</script>
+
+<style>
+.highcharts-container {
+  width: 400px;
+  height: 200px;
+  border: 1px solid #ddd;
+  margin: auto;
+}
+</style>
